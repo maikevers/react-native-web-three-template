a5762db332882c360aa4402a86aeb98216bc841e
Maik Evers <maik.evers@gmail.com>
2020-01-26 15:56:10 +0100

    install three and expo-three

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Expo from "expo";
+import React, { Component } from "react";
+import * as THREE from "three";
+import ExpoTHREE from "expo-three"
 
 export default function App() {
   return (
