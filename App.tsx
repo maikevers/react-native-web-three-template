96466e662bbda2e6a498d497e511f5da09b77f07
Maik Evers <maik.evers@gmail.com>
2020-01-27 15:10:21 +0100

    add eslint, set tsconfig strict

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
-import { View as GraphicsView } from 'expo-graphics';
-import ExpoTHREE, { THREE } from 'expo-three';
-import React from 'react';
+//import { View as GraphicsView } from "expo-graphics";
+import ExpoTHREE, { THREE } from "expo-three";
+import React from "react";
+const ExpoGraphics = require("expo-graphics");
+const GraphicsView = ExpoGraphics.View;
 
 export default class App extends React.Component {
-  renderer: ExpoTHREE.Renderer;
-  scene: ExpoTHREE.THREE.Scene;
-  camera: ExpoTHREE.THREE.PerspectiveCamera;
-  cube: ExpoTHREE.THREE.Mesh;
-  constructor(props){
+  renderer: ExpoTHREE.Renderer | undefined;
+  scene: ExpoTHREE.THREE.Scene | undefined;
+  camera: ExpoTHREE.THREE.PerspectiveCamera | undefined;
+  cube: ExpoTHREE.THREE.Mesh | undefined;
+  constructor(props: Readonly<{}>) {
     super(props);
   }
   componentDidMount() {
@@ -31,19 +33,25 @@ export default class App extends React.Component {
     canvas,
     width,
     height,
-    scale: pixelRatio,
+    scale: pixelRatio
+  }: {
+    gl: WebGLRenderingContext;
+    canvas: any;
+    width: number;
+    height: number;
+    scale: number;
   }) => {
     this.renderer = new ExpoTHREE.Renderer({ gl, pixelRatio, width, height });
-    this.renderer.setClearColor(0xffffff)
+    this.renderer.setClearColor(0xffffff);
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
     this.camera.position.z = 5;
     const geometry = new THREE.BoxGeometry(1, 1, 1);
 
     const material = new THREE.MeshPhongMaterial({
-      color: 0xff0000,
+      color: 0xff0000
     });
-    
+
     this.cube = new THREE.Mesh(geometry, material);
     this.scene.add(this.cube);
 
@@ -54,9 +62,11 @@ export default class App extends React.Component {
     this.scene.add(light);
   };
 
-  onRender = delta => {
-    this.cube.rotation.x += 3.5 * delta;
-    this.cube.rotation.y += 2 * delta;
-    this.renderer.render(this.scene, this.camera);
+  onRender = (delta: number) => {
+    if (this.cube && this.renderer && this.scene && this.camera) {
+      this.cube.rotation.x += 3.5 * delta;
+      this.cube.rotation.y += 2 * delta;
+      this.renderer.render(this.scene, this.camera);
+    }
   };
 }
